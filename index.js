631ef21a1002788dad033cfdc34fd9ad2089d9ff
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2018-07-03 12:15:06 +0530

    change report gen logic

--- index.js
+++ index.js
@@ -3,23 +3,42 @@
 const html = require('./pa11y-reporter-wso2');
 const pa11y = require('pa11y');
 const fs = require('fs');
+const toml = require('toml-js');
 
-runExample();
+//Read the config file
+let parsedConfig = {};
+//build the URL list
+let htmlURLs = '';
+
+fs.readFile('./config/conf.toml', function (err, data) {
+    if(err) {
+        return console.log(err);
+    }
+    parsedConfig = toml.parse(data);
+    //call the report generator
+    generateReport();
+});
+
 
 // Async function required for us to use await
-async function runExample() {
+async function generateReport() {
+
+    //get the page Script
     let pageScriptContents = fs.readFileSync("page_script/index.json");
     let jsonContent = JSON.parse(pageScriptContents);
-    let htmlURLs = '';
+    //get config values
+    let includeNotices = parsedConfig.includeNotices;
+    let includeWarnings = parsedConfig.includeWarnings;
+    let standard = parsedConfig.standard;
 
-    for(var jsonURL in jsonContent ){
+    for(const jsonURL  in jsonContent ){
 
         try {
            const result = await pa11y(jsonContent[jsonURL].pageURL, {
-                "includeNotices": true,
-                "includeWarnings": true,
-                "standard": "Section508",
-                "screenCapture" : `${__dirname}` + "/output/captures/"+jsonContent[jsonURL].taskNo+".png",
+                "includeNotices": includeNotices,
+                "includeWarnings": includeWarnings,
+                "standard": standard,
+                "screenCapture" : `${__dirname}` + "/output/captures/"+jsonURL+".png",
                 "chromeLaunchConfig": {
                     "ignoreHTTPSErrors": true,
                     "headless": false,
@@ -33,14 +52,13 @@ async function runExample() {
                 "actions": jsonContent[jsonURL].actions
             });
 
-            result.imageURL = jsonContent[jsonURL].taskNo + ".png";
+            result.imageURL = jsonURL + ".png";
             result.description = jsonContent[jsonURL].description;
 
             //get the page HTML content
             const htmlResults = await html.results(result);
-            const fileName = jsonContent[jsonURL].taskNo +"_" + result.documentTitle.replace(/ /g,"_") + ".html";
-            htmlURLs += `<li>\n<a href='${fileName}'>${result.documentTitle}</a>\n</li>\n`;
-           // htmlURLs += `<a href='${fileName}'>${fileName}</a><br />\n`;
+            const fileName = jsonURL +"_" + result.documentTitle.replace(/ /g,"_") + ".html";
+            htmlURLs += `<li>\n<a href='${fileName}'>${jsonContent[jsonURL].taskName}</a>\n</li>\n`;
 
             //write the file
             fs.writeFile("./output/" + fileName , htmlResults, function(err) {
@@ -58,8 +76,19 @@ async function runExample() {
         }
     }
 
+    generateIndex();
+
+}
+
+async function generateIndex(){
+    let indexTitle = {};
+    indexTitle.htmlURLs = htmlURLs;
+    indexTitle.standard = parsedConfig.standard;
+    indexTitle.product = parsedConfig.product;
+    indexTitle.version = parsedConfig.version;
+
     //get the index page HTML content
-    const htmlResults = await html.index(htmlURLs);
+    const htmlResults = await html.index(indexTitle);
 
     //write the file
     fs.writeFile("./output/index.html" , htmlResults, function(err) {
@@ -68,5 +97,4 @@ async function runExample() {
         }
         console.log("The file index.html was saved!");
     });
-
 }
